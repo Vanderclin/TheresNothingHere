0817839b19fb0f09035ec44e50afc5c2d5726d6a
Vanderclin Rocha <vanderclinrocha@gmail.com>
2022-07-17 05:13:16 -0300

    New Update

---
+++ js/theresnothinghere-sw.js
@@ -0,0 +1,58 @@
+const dataCacheName = 'pwa-test-data';
+const cacheName = 'pwa-test';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/apple-touch-icon.png',
+  '/assets/astronaut.png',
+  '/assets/favicon.ico',
+  '/assets/icon-192.png',
+  '/assets/icon-192-maskable.png',
+  '/assets/icon-512-maskable.png',
+  '/assets/icon-512-maskable.png',
+  '/css/bootstrap.css',
+  '/css/theresnothinghere.css',
+  '/js/vendor/jquery-3.6.0.min.js',
+  '/js/vendor/popper.min.js',
+  '/js/bootstrap.js',
+  '/js/bootstrap.js.map',
+  '/js/theresnothinghere.js',
+  '/js/theresnothinghere-sw.js'
+];
+
+//install the sw
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function (e) {
+    console.log('[Service Worker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
--- js/theresnothinghere.js
+++ js/theresnothinghere.js
@@ -1,3 +1,40 @@
+window.addEventListener('load', async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register('js/theresnothinghere-sw.js');
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            const notificationPermission = await Notification.requestPermission();
+            if (notificationPermission !== 'granted') {
+                console.log('Notification permission not granted');
+            } else {
+                const title = 'Welcome to the there\'s nothing here...';
+                const options = {
+                    body: 'Do you like this ?',
+                    icon: '../assets/astronaut.png',
+                    vibrate: [200, 100, 200, 100, 200, 100, 400],
+                    tag: 'request',
+                    actions: [
+                        {
+                            action: 'yes',
+                            title: 'Yes',
+                            icon: '../assets/astronaut.png'
+                        },
+                        {
+                            action: 'no',
+                            title: 'No',
+                            icon: '../assets/astronaut.png'
+                        }
+              ]
+                };
+                const res = await registration.showNotification(title, options);
+                console.log(res);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    }
+});
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
